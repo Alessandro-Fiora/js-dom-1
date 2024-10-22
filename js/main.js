915715fc09a265b556ca96bb5fe206e21ae5eb44
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-22 14:05:56 +0200

    accendi e spegni

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("JS OK");
+const buttonEl = document.querySelector("button");
+
+const imgEl = document.querySelector("img");
+
+buttonEl.addEventListener("click", () => {
+  if (imgEl.classList.contains("spenta")) {
+    imgEl.setAttribute("src", "./img/yellow_lamp.png");
+    imgEl.classList = "accesa";
+  } else {
+    imgEl.setAttribute("src", "./img/white_lamp.png");
+    imgEl.classList = "spenta";
+  }
+});
